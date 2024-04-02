e49b37cdc5cca8033566212ed780ffeca7e4bead
RayanaGeeks <r20148@gmail.com>
2024-04-02 21:58:09 +0600

    exam

---
+++ exam/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import TodoPage from "./pages/todoPage/TodoPage";
+
+
+function App() {
+
+    return (
+        <div className="App">
+            <TodoPage/>
+        </div>
+    );
+}
+
+
+export default App;
